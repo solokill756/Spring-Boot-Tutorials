b8fb89760ae61b087383aa59a806532377b22bb3
Thao <hosithao1622004@gmail.com>
2024-08-03 22:13:24 +0700

    Day 3

--- src/main/java/com/springtutorial/springdemo/HomeController.java
+++ src/main/java/com/springtutorial/springdemo/HomeController.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springtutorial.springdemo.model.Alien;
 import com.springtutorial.springdemo.repository.AlenRepo;
 
-@Controller
+@RestController
 public class HomeController {
 
     @Autowired
@@ -38,16 +41,34 @@ public class HomeController {
     //     System.out.println(aliens);
     //     return mv;
     // }
-    @RequestMapping("/aliens")
-    @ResponseBody
+    @GetMapping("/aliens")
     public List<Alien> getAllAlien() {
         return repo.findAll();
     }
 
-    @RequestMapping("/aliens/{aid}")
-    @ResponseBody
+    @PostMapping("/addalien")
+    public Alien addAlien(@RequestBody Alien alien) {
+        //TODO: process POST request
+        repo.save(alien);
+        return alien;
+    }
+
+    @GetMapping("/aliens/{aid}")
     public Optional<Alien> getAllAlien(@PathVariable("aid") int aid) {
         return repo.findById(aid);
+    }
+
+    @DeleteMapping("/DeleteAlien/{aid}")
+    public String deletAlien(@PathVariable("aid") int aid) {
+        repo.deleteById(aid);
+        return "Delete Success";
+    }
+
+    @PutMapping("/alien")
+    public Alien SaveOrUppdateAlien(@RequestBody Alien alien) {
+        //TODO: process PUT request
+        repo.save(alien);
+        return alien;
     }
 
 }
